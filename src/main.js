2b05d14215713b61b64f1d64e714e7a119f1d6d0
DESKTOP-EBVP9FP\kdh <mdiu2203@naver.com>
2021-10-19 14:11:38 +0900

    채널톡 플러그인 추가

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from '@/routes/index';
+import channel from './channel';
 
 Vue.config.productionTip = false;
+Vue.use(channel);
 
 new Vue({
   render: h => h(App),
